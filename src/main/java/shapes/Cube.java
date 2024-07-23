a6de95289393e4e6c29e022f1df93d6235f18686
Cevdet Çağdaş Köse <chatonode@gmail.com>
2024-07-23 16:11:25 +0100

    feat: added calculation of volume of cube

--- src/main/java/shapes/Cube.java
+++ src/main/java/shapes/Cube.java
@@ -3,16 +3,19 @@ package shapes;
 public class Cube extends Shape3D{
 
     public Cube(int size) {
+//        this(size * size); // new Cube()
 
+        new Cube(size * size);
     }
 
     private Cube(Rectangle base) {
-
+       this.base = base;
     }
 
     @Override
     public double calculateVolume() {
+        Rectangle rect = (Rectangle) this.base;
 
-        return 0;
+        return Math.pow(rect.length, 3);
     }
 }
